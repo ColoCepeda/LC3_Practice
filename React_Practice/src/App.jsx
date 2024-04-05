ddd41566028aabcdec42e090108b1380066523ba
Marcelo <marcelo.cepeda17@gmail.com>
2024-04-05 18:54:08 -0300

    Books web advance

--- React_Practice/src/App.jsx
+++ React_Practice/src/App.jsx
@@ -1,14 +1,22 @@
 import "./App.css";
 import BookItem from "./components/bookItem/BookItem";
+import books from "./components/bookArray/BookArray";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h2>Books Champion App</h2>
-      <p>¡Quiero leer libros!</p>
-      <BookItem />
+    <div className="d-flex justify-content-center flex-wrap">
+      {books.map((book, index) => (
+        <BookItem
+          key={index}
+          title={book.bookTitle}
+          author={book.bookAuthor}
+          pageCount={book.pageCount}
+          rating={book.bookRating}
+          imageUrl={book.imageUrl}
+        />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
--- React_Practice/src/components/bookItem/BookItem.jsx
+++ React_Practice/src/components/bookItem/BookItem.jsx
@@ -1,4 +1,32 @@
-const BookItem = () => {
-  return <div>BookItem</div>;
+import { Card } from "react-bootstrap";
+import PropTypes from "prop-types";
+
+const BookItem = ({ title, author, pageCount, rating, imageUrl }) => {
+  return (
+    <div>
+      <Card className="mx-3" style={{ width: "22rem" }}>
+        <Card.Img
+          height={400}
+          variant="top"
+          src={imageUrl !== "" ? imageUrl : "https://bit.ly/47NylZk"}
+        />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Subtitle>{author}</Card.Subtitle>
+          <div>{rating?.length} estrellas</div>
+          <p>{pageCount} páginas</p>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
+
+BookItem.propTypes = {
+  title: PropTypes.string,
+  author: PropTypes.string,
+  pageCount: PropTypes.number,
+  rating: PropTypes.array,
+  imageUrl: PropTypes.string,
+};
+
 export default BookItem;
